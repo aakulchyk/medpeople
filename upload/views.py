592b5fddfb7b246ddaf21679f806c235338a75d4
deadletter <ales.akukchyk@gmail.com>
2016-11-20 19:58:19 +0300

    implemented searching by tags

--- upload/views.py
+++ upload/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.edit import FormView
 from .forms import UploadForm
 from .models import Document
+from dictionary.models import MedicalTerm
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -17,6 +18,7 @@ from django.http import FileResponse
 
 pending_pdfs_list = []
 
+
 @login_required
 def done(request):
     l = pending_pdfs_list[:]
@@ -25,39 +27,60 @@ def done(request):
     ocrThread.start()
 
     template_name = 'upload/done.html'
-    initial = {'file_list_string' : ','.join(l)}
+    initial = {'file_list_string': ','.join(l)}
     return render(request, template_name, initial)
+
 
 @login_required
 def pdf_view(request, document_id):
     return FileResponse(open(document_id, 'rb'), content_type='application/pdf')
 
+
 @login_required
 def reindex_files(request):
     analyzeThread = AnalyzeThread('')
     analyzeThread.start()
-    return HttpResponseRedirect(reverse('upload:index'));
+    return HttpResponseRedirect(reverse('upload:index'))
+
 
 def logout_user(request):
     logout(request)
-    return HttpResponseRedirect(reverse('upload:index'));
+    return HttpResponseRedirect(reverse('upload:index'))
+
 
 class UploadView(LoginRequiredMixin, FormView):
     template_name = 'upload/form.html'
     form_class = UploadForm
     success_url = 'done/'
 
+    def strip_filters(self, filter_names):
+        filters = []
+        for f in filter_names:
+            for found in MedicalTerm.objects.filter(name=f):
+                filters.append(found)
+        if not filters:
+            filters.append(MedicalTerm())
+        return filters
+
     def get(self, request, *args, **kwargs):
         self.request = request
-        objects = Document.objects.filter(user = request.user)
-        return render(request, self.template_name, {'form': self.form_class, 'document_list' : objects.order_by('-visit_date')[:20]})
+        query = Document.objects.filter(user=request.user)
+        if request.GET.get("filters"):
+            filters = self.strip_filters(request.GET["filters"].split(','))
+            for f in filters:
+                query = query.filter(tags=f)
+
+        docs = query.order_by('-visit_date')[:20]
+        request_dict = {'form': self.form_class, 'document_list': docs}
+        return render(request, self.template_name, request_dict)
 
     def form_valid(self, form):
         for each in form.cleaned_data['attachments']:
             file_path = 'attachments/' + each.name
             # if no documents with this name
-            if (not Document.objects.filter(file_attached=file_path)):
-                Document.objects.create(file_attached=each, user = self.request.user)
+            o = Document.objects
+            if (not o.filter(file_attached=file_path)):
+                o.create(file_attached=each, user=self.request.user)
                 pending_pdfs_list.append(file_path)
             else:
                 print(file_path + ' exists!')
