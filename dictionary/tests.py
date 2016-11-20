edee79a86eeaf814bad8f9b5f4558afd3139d3ec
deadletter <ales.akukchyk@gmail.com>
2016-11-20 20:57:23 +0300

    added tests

--- dictionary/tests.py
+++ dictionary/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from dictionary.models import MedicalTerm
 
-# Create your tests here.
+
+class MedicalTermTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.o = MedicalTerm.objects
+        cls.terms = [
+            cls.o.create(name=u'кровь'),
+            cls.o.create(name=u'молоко'),
+            cls.o.create(name=u'кишки')
+        ]
+
+    def test_get_filters_by_names(self):
+        names = ['кровь', 'молоко']
+        self.assertEqual(MedicalTerm.objectsByNames(names),
+                         [self.terms[0], self.terms[1]])
